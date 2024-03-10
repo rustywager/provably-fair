f650ede7df0aa6c04719c53af1147ec13740d26e
Kevin Rasmusson <kevin@jkmholding.com>
2024-03-10 15:05:41 +0100

    added test values

--- fair.js
+++ fair.js
@@ -1,9 +1,10 @@
 import crypto from 'crypto';
 
-const hash = "";
-const publicSeed = "";
-const secret = "";
-const winningPercentage = "";
+// Replace the following values with the ones you want to test
+const hash = "9555b43f10973a1803fddef1e6495994a8040b738f89867f8bc826ba9979b028";
+const publicSeed = "674b35c58fea822b3ea195fe1891bcfd";
+const secret = "8d2a3dc171144e106abf94b21e514bf123d8f0489a440cbf0b5a7cae934032b3";
+const winningPercentage = 9.4048256;
 
 const testHash = crypto.createHash('sha256').update(secret + '-' + publicSeed).digest('hex');
 
